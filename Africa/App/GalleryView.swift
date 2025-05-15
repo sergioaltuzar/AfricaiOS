296250200a1e9c2584bfcc4c3699334ca51f818c
sergio altuzar <sergio.altuzar@gmail.com>
2025-05-15 12:29:32 -0600

    add grid in gallery

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,21 +9,48 @@ import SwiftUI
 
 struct GalleryView: View {
     //: Mark - PROPERTIES
+    @State private var selectedAnimal: String = "lion"
     
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+
+    // EFFICIENTGRID DEFINITION
     
-    // SIMPLE GRID DEFINITION
-    let gridLayout: [GridItem] = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumns: Int = 3
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: (gridColumns))
+    }
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack (alignment: .center, spacing: 30){
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    } //: Loop
+                } //: GRID
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: Scroll
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        //.background(MotionAnimationView())
+        .background(MotionAnimationView())
     }
 }
 
